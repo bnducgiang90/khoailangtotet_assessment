6c9feb2acb75e95b3af45f71d5d08bd21ca964e4
Trinh Giang <bnducgiang90@gmail.com>
2020-04-06 17:22:17 +0700

    env commit

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\10.Python\khoailangtotet\env
+set VIRTUAL_ENV=E:\10.Python\git_svn\khoailangtotet_assessment\env
 
 if not defined PROMPT set PROMPT=$P$G
 
